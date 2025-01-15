6a63f459472d6cfae6cc6182f6e8ce7eb26a9574
Stella Liao <stellaliao@Stellas-MacBook-Pro.local>
2025-01-15 14:47:47 -0500

    Add tetromino shapes

---
+++ src/components/Tetrominoes.js
@@ -0,0 +1,65 @@
+export const TETROMINOES = {
+  I: {
+    shape: [
+      [0, 'I', 0, 0],
+      [0, 'I', 0, 0],
+      [0, 'I', 0, 0],
+      [0, 'I', 0, 0],
+    ],
+    color: 'blue',
+  },
+  J: {
+    shape: [
+      [0, 'J', 0],
+      [0, 'J', 0],
+      ['J', 'J', 0],
+    ],
+    color: 'green',
+  },
+  L: {
+    shape: [
+      [0, 'L', 0],
+      [0, 'L', 0],
+      [0, 'L', 'L'],
+    ],
+    color: 'orange',
+  },
+  O: {
+    shape: [
+      ['O', 'O'],
+      ['O', 'O'],
+    ],
+    color: 'yellow',
+  },
+  S: {
+    shape: [
+      [0, 'S', 'S'],
+      ['S', 'S', 0],
+      [0, 0, 0],
+    ],
+    color: 'red',
+  },
+  T: {
+    shape: [
+      [0, 0, 0],
+      ['T', 'T', 'T'],
+      [0, 'T', 0],
+    ],
+    color: 'red',
+  },
+  Z: {
+    shape: [
+      ['Z', 'Z', 0],
+      [0, 'Z', 'Z'],
+      [0, 0, 0],
+    ],
+    color: 'purple',
+  },
+};
+
+export const randomTetromino = () => {
+  const tetrominoes = 'IJLOSTZ';
+  const randTetromino =
+    tetrominoes[Math.floor(Math.random() * tetrominoes.length)];
+  return TETROMINOES[randTetromino];
+};
