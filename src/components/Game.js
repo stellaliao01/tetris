282098db72f1dbeefab786ac105df82cc0a16ecc
Stella Liao <stellaliao@Stellas-MacBook-Pro.local>
2025-01-15 14:48:56 -0500

    Add random tetromino and drop functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Board from './Board';
+import { randomTetromino } from './Tetrominoes';
 import { DEFAULT_ROW, DEFAULT_COL } from '../utils/constants';
 
 const createBoard = () =>
@@ -7,15 +8,38 @@ const createBoard = () =>
 
 export const Game = () => {
   const [board, setBoard] = useState(createBoard());
+  const [tetromino, _setTetromino] = useState(randomTetromino());
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const dropTetromino = () => {
+    setPosition((prev) => ({
+      x: prev.x,
+      y: prev.y + 1,
+    }));
+  };
 
   const updateBoard = () => {
     const newBoard = createBoard();
+    tetromino.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          newBoard[y + position.y][x + position.x] = value;
+        }
+      });
+    });
     setBoard(newBoard);
   };
 
   useEffect(() => {
-    updateBoard();
+    const interval = setInterval(() => {
+      dropTetromino();
+    }, 1000);
+    return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    updateBoard();
+  }, [tetromino, position]);
 
   return (
     <div>
